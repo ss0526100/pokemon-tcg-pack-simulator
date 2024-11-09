78d791b125073e1303a13bbbac7ae90e9fe313fa
ss0526100 <ss0526100@gmail.com>
2024-11-09 20:10:10 +0900

    feat:메인 레이아웃 변경

--- frontend/src/layouts/MainLayout/MainLayout.style.ts
+++ frontend/src/layouts/MainLayout/MainLayout.style.ts
@@ -1,10 +1,15 @@
 import { css } from '@emotion/react';
 
 export const layout = css`
+  display: flex;
   position: fixed;
   left: 50%;
   transform: translate(-50%, 0);
+  width: 100%;
   max-width: 600px;
   min-height: 100vh;
   border: solid 1px black;
+  flex-direction: column;
+  padding: 2rem;
+  align-items: center;
 `;
