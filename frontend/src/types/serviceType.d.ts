2eb05a914ff36fd0dd1613548c5d2cd5ebf5d6e2
ss0526100 <ss0526100@gmail.com>
2024-11-09 13:10:42 +0900

    feat: 카드 상수 구현

--- frontend/src/types/serviceType.d.ts
+++ frontend/src/types/serviceType.d.ts
@@ -1,1 +1,30 @@
 type RareGrade = 'crown' | 's3' | 's2' | 's1' | 'r4' | 'r3' | 'r2' | 'r1';
+
+type Expansion = 'A1' | 'Promo';
+
+type PokemonCategory = 'normal' | 'ev1' | 'ev2' | 'ex';
+type TrainersCategory = 'supporter' | 'item' | 'item-fossil';
+
+type Category = PokemonCategory | TrainersCategory;
+
+// type PokemonType =
+//   | 'Grass'
+//   | 'Fire'
+//   | 'Water'
+//   | 'Lightning'
+//   | 'Psychic'
+//   | 'Fighting'
+//   | 'Darkness'
+//   | 'Metal'
+//   | 'Dragon'
+//   | 'Colorless'
+//   | 'nonPokemon';
+
+interface Card {
+  id: string | number;
+  cardName: string;
+  imgSrc: string;
+  grade: RareGrade;
+  expansion: Expansion;
+  category: Category;
+}
