9b2d76d2208cb22dc138684685e237e88b4ab6c2
ss0526100 <ss0526100@gmail.com>
2024-11-10 18:45:44 +0900

    feat:통계 svg 추가

---
+++ frontend/src/components/svgs/Statistics.tsx
@@ -0,0 +1,38 @@
+interface StatisticsProps {
+  size?: number;
+  fill?: string;
+}
+
+export default function Statistics(props: StatisticsProps) {
+  const { size = 20, fill = 'white', ...restProps } = props;
+
+  return (
+    <svg
+      version='1.1'
+      id='Capa_1'
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 294 294'
+      xmlSpace='preserve'
+      fill={fill}
+      width={`${size}px`}
+      height={`${size}px`}
+      {...restProps}
+    >
+      <g>
+        <path d='M279,250H15c-8.284,0-15,6.716-15,15s6.716,15,15,15h264c8.284,0,15-6.716,15-15S287.284,250,279,250z' />
+        <path
+          d='M30.5,228h47c5.247,0,9.5-4.253,9.5-9.5v-130c0-5.247-4.253-9.5-9.5-9.5h-47c-5.247,0-9.5,4.253-9.5,9.5v130
+     C21,223.747,25.253,228,30.5,228z'
+        />
+        <path
+          d='M123.5,228h47c5.247,0,9.5-4.253,9.5-9.5v-195c0-5.247-4.253-9.5-9.5-9.5h-47c-5.247,0-9.5,4.253-9.5,9.5v195
+     C114,223.747,118.253,228,123.5,228z'
+        />
+        <path
+          d='M216.5,228h47c5.247,0,9.5-4.253,9.5-9.5v-105c0-5.247-4.253-9.5-9.5-9.5h-47c-5.247,0-9.5,4.253-9.5,9.5v105
+     C207,223.747,211.253,228,216.5,228z'
+        />
+      </g>
+    </svg>
+  );
+}
