631d8f111cb9de08c51924e689b0938b423491be
ss0526100 <ss0526100@gmail.com>
2024-11-10 14:46:53 +0900

    feat:packOpen 재오픈 이름 변경

--- frontend/src/components/PackOpen/PackOpen.tsx
+++ frontend/src/components/PackOpen/PackOpen.tsx
@@ -84,7 +84,9 @@ export default function PackOpen(props: PackOpenProps) {
       <BottomButtonContainer>
         {cardIndex === cardInfos.length - 1 && (
           <Button css={S.buttonAnimation} primary onClick={reopen}>
-            다시 개봉하기({packMapper[nowPackType]}팩)
+            다시 개봉하기(
+            {packMapper[nowPackType] + ' ' + Math.floor(cardInfos.length / 5)}
+            팩)
           </Button>
         )}
         <Button
