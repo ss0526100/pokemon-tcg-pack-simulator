46a86fe5aea54154eb9b57fa4e265e87ff5865c9
ss0526100 <ss0526100@gmail.com>
2024-11-10 14:30:16 +0900

    feat: 10팩 개봉 재개봉 구현

--- frontend/src/components/PackSimulator/PackSimulator.tsx
+++ frontend/src/components/PackSimulator/PackSimulator.tsx
@@ -9,19 +9,39 @@ import getRandomPack from '../../utils/getRandomPack';
 const initPack = getRandomPack();
 
 type Phase = 'select' | 'open' | 'result';
+const getTenPack = (packType: A1PackType) => {
+  const randomPack = new Array(10)
+    .fill(null)
+    .map(() => getRandomPack(packType))
+    .flat();
+
+  return randomPack;
+};
 export default function PackSimulator() {
   const [cardPack, setCardPack] = useState<CardInfo[]>(initPack);
   const [nowPackType, setNowPackType] = useState<A1PackType>('charizard');
+  const [openPackCnt, setOpenPackCnt] = useState<1 | 10>(1);
   const [phase, setPhase] = useState<Phase>('select');
+
   const goOpenPhase = useCallback((packType: A1PackType) => {
+    setOpenPackCnt(1);
     const randomPack = getRandomPack(packType);
     setCardPack(randomPack);
     setPhase('open');
     setNowPackType(packType);
   }, []);
 
+  const goOpenPhaseWithTen = useCallback((packType: A1PackType) => {
+    setOpenPackCnt(10);
+    const randomPack = getTenPack(packType);
+    setCardPack(randomPack);
+    setPhase('open');
+    setNowPackType(packType);
+  }, []);
+
   const reopen = () => {
-    const randomPack = getRandomPack(nowPackType);
+    const randomPack =
+      openPackCnt === 1 ? getRandomPack(nowPackType) : getTenPack(nowPackType);
     setCardPack(randomPack);
   };
 
@@ -29,22 +49,13 @@ export default function PackSimulator() {
     setPhase('select');
   };
 
-  const onTenPackOpen = (packType: A1PackType) => {
-    const randomPack = new Array(10)
-      .fill(null)
-      .map(() => getRandomPack(packType))
-      .flat();
-    setCardPack(randomPack);
-    setPhase('open');
-    setNowPackType(packType);
-  };
   return (
     <section css={S.layout}>
       {phase === 'select' && (
         <PackSelect
           onSelect={goOpenPhase}
+          onTenSelect={goOpenPhaseWithTen}
           startPackType={nowPackType}
-          onTenSelect={onTenPackOpen}
         />
       )}
       {phase === 'open' && (
