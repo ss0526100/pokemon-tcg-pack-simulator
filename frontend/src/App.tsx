08f9fa87777e6d39a64a150565b7c1e4719b99aa
ss0526100 <ss0526100@gmail.com>
2024-11-14 17:54:00 +0900

    feat:프리패치 비활성화

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,12 +6,13 @@ import Article from './pages/Article/Article';
 import ErrorPage from './pages/ErrorPage/ErrorPage';
 import GetChallenge from './pages/GetChallenge/GetChallenge';
 import { Global } from '@emotion/react';
-import ImportImages from './pages/PackSimulator/importImages';
 import MainLayout from './layouts/MainLayout/MainLayout';
 import PackSimulator from './pages/PackSimulator/PackSimulator';
 import { RecoilRoot } from 'recoil';
 import reset from './reset.style';
 import { useEffect } from 'react';
+
+// import ImportImages from './pages/PackSimulator/importImages';
 
 function isKakaoBrowser() {
   const userAgent = navigator.userAgent.toLowerCase();
@@ -42,7 +43,7 @@ const router = createBrowserRouter([
 const effectKeys = [' '];
 function App() {
   useEffect(() => {
-    ImportImages();
+    // ImportImages(); 이후 활성화
     function handleKeyDown(event: KeyboardEvent) {
       if (effectKeys.includes(event.key)) {
         event.preventDefault();
