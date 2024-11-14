462d9aaf3595bc7311d88248580fa474befbaede
ss0526100 <ss0526100@gmail.com>
2024-11-14 17:01:29 +0900

    feat:i18n 감지 순서 navigator로 수정

--- frontend/src/locales/i18n.ts
+++ frontend/src/locales/i18n.ts
@@ -28,6 +28,10 @@ i18n
     interpolation: {
       escapeValue: false,
     },
+    detection: {
+      order: ['navigator'],
+      caches: ['localStorage'],
+    },
   });
 
 export default i18n;
