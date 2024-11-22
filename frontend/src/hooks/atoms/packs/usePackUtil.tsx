1887f162d9a667d7a57b0bea4c83f4ab59af8b1e
ss0526100 <ss0526100@gmail.com>
2024-11-22 22:12:24 +0900

    feat:가지고 있는 카드 훅 구현

--- frontend/src/hooks/atoms/packs/usePackUtil.tsx
+++ frontend/src/hooks/atoms/packs/usePackUtil.tsx
@@ -1,6 +1,7 @@
 import increaseCntMap from '../../../utils/increaseMap';
 import { useCallback } from 'react';
 import useCardCount from './useCardCount';
+import useCardIdCntMap from './useCardCollections';
 import useOpenedPackCount from './useOpenedPackCount';
 import useRarityCntMap from './useRarityCntMap';
 
@@ -8,6 +9,7 @@ export default function usePackUtil() {
   const setOpenedPackCount = useOpenedPackCount()[1];
   const setGottenCardCount = useCardCount()[1];
   const setRarityCntMap = useRarityCntMap()[1];
+  const setCardCntMap = useCardIdCntMap()[1];
 
   const countCard = useCallback(
     (...cardInfos: CardInfo[]) => {
@@ -17,8 +19,13 @@ export default function usePackUtil() {
         cardInfos.forEach(cardInfo => increaseCntMap(nextMap, cardInfo.rarity));
         return nextMap;
       });
+      setCardCntMap(prevMap => {
+        const nextMap = new Map([...prevMap]);
+        cardInfos.forEach(cardInfo => increaseCntMap(nextMap, cardInfo.id));
+        return nextMap;
+      });
     },
-    [setGottenCardCount, setRarityCntMap]
+    [setGottenCardCount, setRarityCntMap, setCardCntMap]
   );
 
   const increasePackCount = useCallback(
