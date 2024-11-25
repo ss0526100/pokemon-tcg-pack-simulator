b4b5b6cbad14b57563b9c05a9fe2ee7bca6b4309
ss0526100 <ss0526100@gmail.com>
2024-11-25 21:07:33 +0900

    feat:카드 곡선 줌

--- frontend/src/components/Card/Card.style.tsx
+++ frontend/src/components/Card/Card.style.tsx
@@ -2,7 +2,10 @@ import { css } from '@emotion/react';
 
 export const card = css`
   transform: translateZ(0); /* iOS의 렌더링 오류 방지 */
+
   aspect-ratio: 1 / 1.395;
   height: 100%;
+
   background-size: cover;
+  border-radius: 11px;
 `;
