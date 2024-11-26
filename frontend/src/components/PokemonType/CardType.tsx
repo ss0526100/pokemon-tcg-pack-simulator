1b8e4ca8f9bcaf1b62502cd302c7c11f4250c100
ss0526100 <ss0526100@gmail.com>
2024-11-26 19:51:43 +0900

    feat:카드 타입 컴포넌트 구현

---
+++ frontend/src/components/PokemonType/CardType.tsx
@@ -0,0 +1,42 @@
+import * as S from './CardType.styles';
+
+import ColorlessPng from '../../assets/types/colorless.png';
+import DarknessPng from '../../assets/types/darkness.png';
+import DragonPng from '../../assets/types/dragon.png';
+import FightingPng from '../../assets/types/fighting.png';
+import FirePng from '../../assets/types/fire.png';
+import GrassPng from '../../assets/types/grass.png';
+import LightningPng from '../../assets/types/lightning.png';
+import MetalPng from '../../assets/types/metal.png';
+import PsychicPng from '../../assets/types/psychic.png';
+import WaterPng from '../../assets/types/water.png';
+
+interface CardTypeProps {
+  pokemonType: PokemonType;
+  size?: number;
+}
+
+const typeSrcRecord: Record<Exclude<PokemonType, 'nonPokemon'>, string> = {
+  Colorless: ColorlessPng,
+  Darkness: DarknessPng,
+  Dragon: DragonPng,
+  Fighting: FightingPng,
+  Fire: FirePng,
+  Grass: GrassPng,
+  Lightning: LightningPng,
+  Metal: MetalPng,
+  Psychic: PsychicPng,
+  Water: WaterPng,
+};
+export default function CardType(props: CardTypeProps) {
+  const { pokemonType, size = 15 } = props;
+
+  return (
+    <div css={S.container}>
+      <img
+        css={S.pokemonType(size)}
+        src={pokemonType === 'nonPokemon' ? '' : typeSrcRecord[pokemonType]}
+      />
+    </div>
+  );
+}
