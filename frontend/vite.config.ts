6a2d7722b54e3a4e6468d8e88bdd9c8ec2d735a1
ss0526100 <ss0526100@gmail.com>
2024-12-03 14:03:56 +0900

    chore:절대경로 설정을 위한 플러그인 설치

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,6 +8,7 @@ export default defineConfig({
     react({
       jsxImportSource: '@emotion/react',
     }),
+    tsconfigPaths(),
   ],
   build: {
     rollupOptions: {
